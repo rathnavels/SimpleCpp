66878e54c0cb96bdebb455ac9e8dec1fc2c7c3cd
Rathnavel Sankaralingam <pixrat92@gmail.com>
2019-04-23 22:52:57 -0500

    Updates

--- Miscallaneous/main.cpp
+++ Miscallaneous/main.cpp
@@ -1,9 +1,4 @@
-/* Enter your macros here */
-#define toStr(a)        string("Result =")
-#define io(v)           std::cin >> v
-#define FUNCTION(name, operator) void name(int &current, int &newValue){ if(!(current operator newValue)) current = newValue;}
-#define INF   INT_MAX
-#define foreach(v, i)  for(int i=0; i<v.size(); i++)
+
 
 
 #include <iostream>
@@ -11,34 +6,9 @@
 #include <string>
 using namespace std;
 
-#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
-#error Missing preprocessor definitions
-#endif 
+int main()
+{
+  
 
-FUNCTION(minimum, <)
-FUNCTION(maximum, >)
-
-int main(){
-
-	int n; cin >> n;
-	vector<int> v(n);
-	foreach(v, i) 
-  {
-		io(v)[i];
-	}
-	int mn = INF;
-	int mx = -INF;
-  int ans = 0;
-
-
-  foreach(v, i)
-  {
-    	minimum(mn, v[i]);
-		  maximum(mx, v[i]);
-  }
-  ans = mx-mn;
-
-	cout << toStr(Result =) <<' '<< ans;
-	
 return 0;
 }
